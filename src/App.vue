7cf286f82b6ba12e9e893cfebc16278a605401b4
Tusko <tusko@photoinside.me>
2019-08-31 18:06:09 +0300

    changed ip locatiin provider

--- src/App.vue
+++ src/App.vue
@@ -5,34 +5,97 @@
       api-key="AIzaSyA_GkVQfPLIp99nbA3tGNH7C1ADQl0ayWQ"
       :lat="coordinates.lat"
       :lng="coordinates.lng"
-      :accuracy="coordinates.radius"
     />
+    <marker-pulse :loading="loading" />
+    <form v-show="!loading" class="lookup" novalidate @submit.prevent="formLookup">
+      <h1>IP Lookup</h1>
+      <div class="form-row">
+        <input
+          v-model="lookup"
+          type="text"
+          minlength="7"
+          maxlength="15"
+          size="15"
+          pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$"
+        />
+        <button type="submit" class="icon-search"></button>
+      </div>
+
+      <h5 v-if="isError">IP Not Found</h5>
+      <h5 v-else>
+        <b>Location:</b>
+        {{ location }}
+      </h5>
+    </form>
+    <footer>
+      <a href="https://arsmoon.com/" target="_blank">Created by Arsmoon</a>
+    </footer>
   </div>
 </template>
 
 <script>
 require("./styles.scss");
 import axios from "axios";
+const ipInfoToken = "cc9000ffb074c6";
+
 export default {
   name: "app",
   components: {
+    "marker-pulse": () => import("./components/markerPulse"),
     "vue-map": () => import("./components/VueMaps")
   },
   data: () => ({
+    location: null,
+    isError: false,
     loading: true,
+    lookup: null,
     coordinates: {
       lat: null,
-      lng: null,
-      radius: null
+      lng: null
     }
   }),
-  beforeCreate() {
-    axios("http://www.geoplugin.net/json.gp?jsoncallback=").then(r => {
-      this.coordinates.lat = r.data.geoplugin_latitude;
-      this.coordinates.lng = r.data.geoplugin_longitude;
-      this.coordinates.radius = r.data.geoplugin_locationAccuracyRadius;
-      this.loading = false;
-    });
+  created() {
+    if (this.$route.query.lookup) {
+      this.lookup = this.$route.query.lookup;
+      this.formLookup(new Event("load"));
+    } else {
+      this.startLookup();
+    }
+  },
+  methods: {
+    startLookup() {
+      this.loading = true;
+      axios(`https://ipinfo.io/?token=${ipInfoToken}`)
+        .then(res => this.dataIPfill(res.data))
+        .finally(() => (this.loading = false));
+    },
+    formLookup(e) {
+      e.preventDefault();
+      this.loading = true;
+
+      axios(`http://ipinfo.io/${this.lookup}/geo?token=${ipInfoToken}`)
+        .then(res => this.dataIPfill(res.data))
+        .finally(() => (this.loading = false));
+    },
+    dataIPfill(o) {
+      if (o.bogon) return (this.isError = true);
+      const crdnts = o.loc.split(",");
+      this.isError = false;
+      this.coordinates.lat = crdnts[0];
+      this.coordinates.lng = crdnts[1];
+      this.lookup = o.ip;
+      this.location =
+        (o.city ? o.city + ", " : "") +
+        (o.region ? o.region + ", " : "") +
+        o.country;
+
+      if (this.$route.query.lookup !== o.ip)
+        this.$router.push({
+          query: {
+            lookup: o.ip
+          }
+        });
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App";
 import LoadScript from "vue-plugin-load-script";
+import VueRouter from "vue-router";
 
 Vue.use(LoadScript);
+Vue.use(VueRouter);
 
 if (process.env.NODE_ENV === "production") {
   Vue.config.productionTip = false;
@@ -11,6 +13,12 @@ if (process.env.NODE_ENV === "production") {
   Vue.config.debug = true;
 }
 
+const router = new VueRouter({
+  mode: "history",
+  routes: [{ path: "/", component: App }]
+});
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount("#app");
